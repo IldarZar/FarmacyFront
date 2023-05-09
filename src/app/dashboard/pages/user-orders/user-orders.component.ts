b0cf4fd3d48e6ce59a53bceae99d9e5cbe97b80b
IldarZar <63116956+IldarZar@users.noreply.github.com>
2023-05-09 15:39:33 +0500

    добавил логику по изменению статусов заявок

--- src/app/dashboard/pages/user-orders/user-orders.component.ts
+++ src/app/dashboard/pages/user-orders/user-orders.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { DashboardService } from '@dashboard/services/dashboard.service';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { UserOrder } from '@shared/models/user-order';
+import { OrderStatus } from '@shared/models/enums/order-status';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Dictionary } from '@core/models/dictionary';
 
 @Component({
   selector: 'app-user-orders',
@@ -9,19 +12,79 @@ import { UserOrder } from '@shared/models/user-order';
   styleUrls: ['./user-orders.component.scss'],
 })
 export class UserOrdersComponent implements OnInit {
+  constructor(
+    private dashboardService: DashboardService,
+    private changeDetector: ChangeDetectorRef
+  ) {}
+
+  formGroup = new FormGroup({
+    userOrders: new FormArray<FormGroup>([]),
+  });
+
+  statuses = [
+    {
+      id: OrderStatus.IN_PROGRESS,
+      name: 'В процессе',
+    },
+    { id: OrderStatus.DONE, name: 'Сделано' },
+    { id: OrderStatus.CANCELLED, name: 'Отменено' },
+    { id: OrderStatus.EXPIRED, name: 'Задержано' },
+  ];
+
   orderHistory$: Observable<UserOrder[]>;
-
-  constructor(private dashboardService: DashboardService) {}
-
   ngOnInit(): void {
     this.orderHistory$ = this.dashboardService.getOrderHistoryByDeliveryPoint();
+
+    this.dashboardService
+      .getOrderHistoryByDeliveryPoint()
+      .pipe(
+        tap((userOrders) => {
+          userOrders.forEach((userOrder: UserOrder) => {
+            console.log(userOrder as UserOrder);
+            this.formGroup.controls['userOrders'].push(
+              new FormGroup({
+                id: new FormControl(userOrder.id),
+                orderDateTime: new FormControl(
+                  userOrder.orderDateTime.toString()
+                ),
+                expectedDate: new FormControl(
+                  userOrder.expectedDate.toString()
+                ),
+                lastStorageDay: new FormControl(userOrder.lastStorageDay),
+                productOrder: new FormControl(userOrder.productOrder),
+                sum: new FormControl(userOrder.sum),
+                deliveryPoint: new FormControl(userOrder.deliveryPoint),
+                status: new FormControl(this.setStatus(userOrder.status)),
+                user: new FormControl(userOrder.user),
+              })
+            );
+          });
+        })
+      )
+      .subscribe();
   }
 
-  setOrderStatus(userOrder: UserOrder) {
-    this.orderHistory$ = this.dashboardService
-      .setOrderStatus(userOrder)
-      .pipe(
-        switchMap(() => this.dashboardService.getOrderHistoryByDeliveryPoint())
-      );
+  private setStatus(status: string | OrderStatus): {
+    id: OrderStatus;
+    name: string;
+  } {
+    switch (status) {
+      case 'IN_PROGRESS':
+        return this.statuses[0];
+      case 'DONE':
+        return this.statuses[1];
+      case 'CANCELLED':
+        return this.statuses[2];
+      default:
+        return this.statuses[3];
+    }
+  }
+
+  statusChanged(newStatus: Dictionary<number>, userOrder: UserOrder) {
+    this.dashboardService.setOrderStatus(userOrder, newStatus).subscribe(() => {
+      this.formGroup.controls['userOrders'].controls
+        .find((el) => el.controls['id'].value === userOrder.id)
+        ?.patchValue({ status: newStatus });
+    });
   }
 }
