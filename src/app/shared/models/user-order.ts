6de3e057e408d3d3217ae1cf001f24f9780a74a0
IldarZar <63116956+IldarZar@users.noreply.github.com>
2023-05-14 19:20:26 +0500

    добавил комбобокс при заказе товара, добавил верстку в user-orders

--- src/app/shared/models/user-order.ts
+++ src/app/shared/models/user-order.ts
@@ -2,6 +2,7 @@ import { ProductOrder } from '@shared/models/product-order';
 import { Identifiable } from '@core/models/identifable';
 import { DeliveryPoint } from '@shared/models/delivery-point';
 import { OrderStatus } from '@shared/models/enums/order-status';
+import { User } from '@shared/models/user/user';
 
 export interface UserOrder extends Identifiable<number> {
   orderDateTime: Date;
@@ -12,5 +13,5 @@ export interface UserOrder extends Identifiable<number> {
   deliveryPoint: DeliveryPoint;
   status: OrderStatus;
   // потому что бэк возвращает только ид
-  user: { id: number };
+  user: User;
 }
