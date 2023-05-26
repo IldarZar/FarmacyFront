1232ffe4cfb9dd866c2c445b666ed9e980b59974
IldarZar <63116956+IldarZar@users.noreply.github.com>
2023-05-27 00:02:34 +0500

    Небольшие правки по стилям

--- src/app/core/interceptors/base-url.interceptor.ts
+++ src/app/core/interceptors/base-url.interceptor.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
-
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     const newReq = request.clone({
-      url: 'http://localhost:8888/apteka-api' + request.url,
+      url:
+        'https://apteka-api-apteka-service.azuremicroservices.io/apteka-api' +
+        request.url,
     });
     return next.handle(newReq);
   }
